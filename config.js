61226cb8fcdef937efe58e2da7801d94ae3e86dd
Tamas Sule <admin@tamassule.com>
2018-10-06 23:44:25 +0200

    connects to solo codes channel and listens for 3 letter game codes

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  discordBotToken: 'NDk3ODU1MDY1ODAyMDE0NzIw.Dpqv6Q.ak0qnGdAi4EBJ63DUQ1W9AksgMs'
+  discordBotToken: 'NDk3ODU1MDY1ODAyMDE0NzIw.Dpqv6Q.ak0qnGdAi4EBJ63DUQ1W9AksgMs',
+  codeChannels: {
+    solo: 'solo-codes'
+  }
 };
--- snipe-bot.js
+++ snipe-bot.js
@@ -1,11 +1,24 @@
 const MessageProcessor = require('./message-processor.js');
 const DMMessageHandler = require('./dm-message-handler.js');
+const config = require('./config');
 
 class SnipeBot {
   
   constructor(client) {
     this._client = client;
     this._messageProcessor = new MessageProcessor();
+    this._lookupChannels();
+    this._askForSoloCodes();
+  }
+
+  _lookupChannels() {
+    this._soloCodesChannel = this._client.channels.find(
+      channel => channel.name === config.codeChannels.solo
+    );
+
+    if(this._soloCodesChannel) {
+      console.log('Solo Codes channel has been registered');
+    }
   }
 
   run() {
@@ -15,11 +28,28 @@ class SnipeBot {
 
   _registerMessageHandlers() {
     this._messageProcessor.registerHandler('dm', new DMMessageHandler());
-    //this._messageProcessor.registerHandler(new ChannelMessageHandler());
   }
 
   _addMessageProcessor() {
     this._client.on('message', this._messageProcessor.process);
+  }
+
+  async _askForSoloCodes() {
+    try {
+      const message = await this._soloCodesChannel.send('Now accepting game codes');
+
+      if(message) {
+        const collector = this._soloCodesChannel.createMessageCollector(
+          message => message.content.length === 3,
+          { time: 20000 } // TODO: move to config
+        );
+    
+        collector.on('collect', message => console.log('Collected', message.content));
+        collector.on('end', collected => console.log(`collected ${collected.size} codes`));
+      }
+    } catch(err) {
+      console.error(err);
+    }
   }
 }
 
