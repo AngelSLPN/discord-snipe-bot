5a7ddb85b74fe038bde0e3b0df736eb99b208ee4
Tamas Sule <admin@tamassule.com>
2018-10-07 00:29:14 +0200

    remove my token from config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  discordBotToken: 'NDk3ODU1MDY1ODAyMDE0NzIw.Dpqv6Q.ak0qnGdAi4EBJ63DUQ1W9AksgMs',
+  discordBotToken: '<your_token>',
   codeChannels: {
     solo: 'solo-codes'
   }
